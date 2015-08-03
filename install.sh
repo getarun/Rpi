6d299ea68a82f42fd6224000793dd9f1e2be9955
Zimmer-web <getarun@users.noreply.github.com>
2015-08-03 12:47:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+sudo apt-get install mysql-server build-essential python-dev
+#mysql -u root -h localhost -p
+#CREATE USER pi@localhost IDENTIFIED BY pi;
 
+###### HTML-Graph darstellung ######
+ sudo apt-get install lighttpd php5-cgi
+ sudo lighttpd-mod-enable fastcgi fastcgi-pph #mod_userdir      #   enable user dir ~/* to accept requests like 
+ sudo service lighttpd force-reload                            #    http://xx.xx.xx.xx/~pi/*
+####################################
+#installiert python-libs und Adafruit DHT22 library
+git clone https://github.com/getarun/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+sudo pyhton setup.py install
 
+git clone https://github.com/getarun/shell-scripte
+cd shell-scripte
+./install-mysql-connector.sh
 
 cd /home/pi/RPi
 sudo cp data.php /var/www
