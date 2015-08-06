a6a848edcc978bffa6fd6178d9875cca514186ab
Zimmer-pi <zimmer@jzo.de>
2015-08-06 20:31:48 +0000

    Lena is Spitze!! <3

--- control.py
+++ control.py
@@ -12,6 +12,7 @@ test_relais = 	"false"
 use_db      = 	"false"
 use_file    = 	"true"
 create_new_db = "false"
+import json
 use_json    =	"true"
 #be verbose! detailliertere fehlermeldungen, 0=normal -- 1=detalliert
 verbose = 1
@@ -342,13 +343,12 @@ def insert_into_file():
 	if verbose == "1":
 		print("Writing values {},{},{},{},{},{},{},{},{},{},{},{} into file".format(timestamp,date,t1,t2,t3,rh1,rh2,rh3,tmax,tmin,absdraussen,absdrinnen))
 	if use_json == "false":
-		file = open("data.list", "a")
-		file.write(timestamp+"\t"+date+"\t"+t1+"\t"+t2+"\t"+t3+"\t"+rh1+"\t"+rh2+"\t"+rh3+"\t"+tmax+"\t"+tmin+"\t"+absdraussen+"\t"+absdrinnen+"\n")
-		file.close()
+		with open("./data.list", "w") as file_list:
+			file_list.write(timestamp+"\t"+date+"\t"+t1+"\t"+t2+"\t"+t3+"\t"+rh1+"\t"+rh2+"\t"+rh3+"\t"+tmax+"\t"+tmin+"\t"+absdraussen+"\t"+absdrinnen+"\n")
 	if use_json == "true":
-		file = open("data.json", "a")
-		file.write(json.dumps([timestamp, t1,t2,t3])
-		file.close()
+		with open("./data.json", "w") as file_json:
+#			old_data = file_json.read()
+			file_json.write(json.dumps([timestamp, t1,t2,t3,rh1,rh2,rh3.absdraussen,absdrinnen]))
 ################### MAIN #########################
 ##Testroutinen
 if test_light == "true":	
@@ -372,6 +372,7 @@ while 1:
 			insert_into_sql()
 		if use_file == "true":
 			insert_into_file()
+
 	except KeyboardInterrupt:
 		print('captured CRTL+C . . . resetting ports ... exiting')
 		GPIO.cleanup()	
