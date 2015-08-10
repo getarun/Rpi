773b30fcbeb7e42e65acbd7d402878c65951d1ed
Zimmer-web <getarun@users.noreply.github.com>
2015-08-10 14:31:55 +0200

    Update update-local-files.sh

--- update-local-files.sh
+++ update-local-files.sh
@@ -2,9 +2,7 @@
 cd /home/pi/git-working-dir/RPi
 
 echo "cp data.php, create-graph.html, about.php to /var/www"
-sudo cp data.php /var/www
-sudo cp create-graph.html /var/www
-sudo cp about.php /var/www
+sudo cp about.php data.php index.hmtml get-watering.php /var/www
 echo "chown www-data:www-data to /var/www"
 sudo chown www-data:www-data /var/www
 
