db96a23732d5c5a24741b7ade09770885e4dda68
Zimmer-web <getarun@users.noreply.github.com>
2015-08-10 15:19:42 +0200

    Update get-watering.php

--- get-watering.php
+++ get-watering.php
@@ -7,7 +7,7 @@ $EC		= $_POST["EC"];
 /* */
   $timestamp = time()*1000+7200	;
   $dbc 		= mysqli_connect("localhost", "pi", "pi", "klima_growbox");
-  $query 	= "INSERT INTO giessen (timestamp,plantnumber,PH,EC) values($timestamp, . $_POST["plantnumber"] ., . $_POST["amount"] ., . $_POST["PH"] ., '" . $_POST["EC"] . "')";
+  $query 	= "INSERT INTO giessen (timestamp,plantnumber,amount,PH,EC) values($timestamp, $_POST["plantnumber"], $_POST["amount"], $_POST["PH"], $_POST["EC"])";
   $insert	= mysqli_query($dbc, $query);
   if (mysqli_affected_rows($insert) > 0) {echo "row inserted"};
 ?>
