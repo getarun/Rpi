fb6587e17d9b451aa86942009901e41bb8b51f34
Zimmer-web <getarun@users.noreply.github.com>
2015-08-10 12:43:55 +0200

    Update update-local-files.sh

--- update-local-files.sh
+++ update-local-files.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 cd /home/pi/git-working-dir/RPi
 
+echo "cp data.php, create-graph.html, about.php to /var/www"
 sudo cp data.php /var/www
 sudo cp create-graph.html /var/www
-sudo cp control.py /usr/bin
 sudo cp about.php /var/www
-
-sudo chmod +x /usr/bin/control.py
-
+echo "chown www-data:www-data to /var/www"
 sudo chown www-data:www-data /var/www
 
+echo "Installing control.py to /usr/bin"
+sudo cp control.py /usr/bin
+echo "Setting execution bit to /usr/bin/control.py"
+sudo chmod +x /usr/bin/control.py
+
 ###installs manpage
-echo "installing man-page"
+echo "Installing man-page"
 gzip manpage.1
 sudo cp manpage.1.gz /usr/share/man/
