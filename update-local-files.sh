760b92807d8ae1745889e773dc7353aa0c24428d
Zimmer-pi <zimmer@jzo.de>
2015-12-10 18:52:21 +0000

    weis nicht mehr genau .-.. hat glaube was mit dem dateienexport zu tun JSON und so ,,,

--- update-local-files.sh
+++ update-local-files.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-cd /home/pi/git-working-dir/RPi
+##################
+#echo "Use this to Copy Lokal Git Repo in RPi to SD"
+#echo "Only use with working versions annotated with tags >= v1.0"
+##################
 
+cd /home/pi/git-working-dir/RPi
+echo "update-local-files: source: /home/pi/git-working-dir/RPi"
+echo "Copying data.php, create-graph.html, about.php to webserver (/var/www)"
 sudo cp data.php /var/www
 sudo cp create-graph.html /var/www
-sudo cp control.py /usr/bin
 sudo cp about.php /var/www
-
-sudo chmod +x /usr/bin/control.py
-
 sudo chown www-data:www-data /var/www
+
+#echo("Copying control.py to local bin-tree (/usr/bin)")
+sudo cp control.py /usr/bin
+sudo chmod +x /usr/bin/control.py
