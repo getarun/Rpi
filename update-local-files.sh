e33ddbf33f0f509792ec29d319cb97c13f17b091
Zimmer-web <getarun@users.noreply.github.com>
2015-12-11 22:45:00 +0100

    Update update-local-files.sh

--- update-local-files.sh
+++ update-local-files.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 cd /home/pi/git-working-dir/RPi
 
-sudo cp data.php /var/www
-sudo cp create-graph.html /var/www
-sudo cp control.py /usr/bin
-sudo cp about.php /var/www
-
-sudo chmod +x /usr/bin/control.py
-
-sudo chown www-data:www-data /var/www
+print "Kopiere rc.local vom RePo nach /etc"
+sudo cp rc.local /etc/rc.local
