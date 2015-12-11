8cab4421161d00849b7a3dd4b2ad11d8a6a11a94
Zimmer-web <getarun@users.noreply.github.com>
2015-12-11 20:37:28 +0100

    Update get-watering.php

--- get-watering.php
+++ get-watering.php
@@ -1,8 +1,8 @@
 <?php
-$plantnumber 	= $_GET["plantnumber"];
-$amount      	= $_GET["amount"];
-$PH		= $_GET["PH"];
-$EC		= $_GET["EC"];
+$plantnumber 	= $_POST["plantnumber"];
+$amount      	= $_POST["amount"];
+$PH		= $_POST["PH"];
+$EC		= $_POST["EC"];
 /*
 $plantnumber 	= 1;
 $amount      	= 500;
@@ -12,7 +12,7 @@ $EC		= 1.2;
 $timestamp 	= time()*1000+7200;
 
   $dbc 		= mysqli_connect("localhost", "pi", "pi", "klima_growbox");
-  $query 	= "INSERT INTO klima_growbox.giessen (timestamp,plantnumber,amount,PH,EC) VALUES ($timestamp, $amount, $PH, $EC)";
+  $query 	= "INSERT INTO klima_growbox.giessen (timestamp,plantnumber,amount,PH,EC) VALUES ($timestamp, $plantnumber, $amount, $PH, $EC)";
   $execute	= mysqli_query($bdc, $query);
   if (mysqli_query($dbc, $query)){
     echo "New record created successfully";
